356ca0ab131482adca938c533272e62bdb38e450
TalisDuarte007 <tcd07@hotmail.com>
2022-05-09 14:37:01 -0300

    atualização no doc main.js. adicionando novas funcionalidades no código

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function embutida(){
+function embutida() {
     const medidaInterna = Number(document.querySelector('.inputMedidaInterna').value);
     const numeroDePortas = Number(document.querySelector('.inputQuantidadePortas').value);
 
@@ -15,7 +15,7 @@ function embutida(){
         document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 2 vãos. Um deles de ${vao1}cm, outro com ${vao2}cm.`;
     }
     else if (numeroDePortas == 4) {
-        const vao1 = (medidaInterna -1.8) / 2;
+        const vao1 = (medidaInterna - 1.8) / 2;
         document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 2 vãos iguais de ${vao1}cm.`;
     }
     else if (numeroDePortas == 5) {
@@ -33,40 +33,55 @@ function embutida(){
         const vao2 = (medidaPortas * 2) + 1.2;
         document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 4 vãos. Um lateral com ${vao1}cm, outros 3 iguais com ${vao2}cm.`;
     }
-    else{
+    else {
         document.querySelector('.resultado').innerHTML = " "
         document.querySelector('.resultadoVaos').innerHTML = "Valor inválido. Por Favor, indique um número válido."
     }
 }
 
-// function nEmbutida(){
-//     const medidaInterna = document.querySelector('.inputMedidaInterna').value;
-//     const numeroDePortas = document.querySelector('.inputQuantidadePortas').value;
+function nEmbutida() {
+    const medidaInterna = Number(document.querySelector('.inputMedidaInterna').value);
+    const numeroDePortas = Number(document.querySelector('.inputQuantidadePortas').value);
 
-//     if (numeroDePortas = 1) {
-        
-//     }
-//     else if (numeroDePortas = 2) {
-        
-//     }
-//     else if (numeroDePortas = 3) {
-        
-//     }
-//     else if (numeroDePortas = 4) {
-        
-//     }
-//     else if (numeroDePortas = 5) {
-        
-//     }
-//     else if (numeroDePortas = 6) {
-        
-//     }
-//     else if (numeroDePortas = 7) {
-        
-//     }
-//     else{
+    const medidaPortas = ((medidaInterna + 1.2) - (0.3 * (numeroDePortas - 1))) / numeroDePortas;
 
-//     }
+    document.querySelector('.resultado').innerHTML = `O móvel terá ${numeroDePortas} portas com ${medidaPortas}cm.`;
 
-//     document.querySelector('.resultado').innerHTML = teste;
-// }
+    if (numeroDePortas == 2) {
+        document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 1 vão de ${medidaInterna}cm.`;
+    }
+
+    else if (numeroDePortas == 3) {
+        const vao1 = medidaPortas - 1.5;
+        const vao2 = (medidaPortas * 2) - 0.9;
+        document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 2 vãos. Um deles de ${vao1}cm, outro com ${vao2}cm.`;
+    }
+
+    else if (numeroDePortas == 4) {
+        const vao1 = (medidaInterna - 1.8) / 2;
+        document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 2 vãos iguais de ${vao1}cm.`;
+    }
+
+    else if (numeroDePortas == 5) {
+        const vao1 = medidaPortas - 1.5;
+        const vao2 = (medidaInterna + 3.6 - vao1) / 2;
+        document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 3 vãos. Um deles com ${vao1}cm, outros 2 com ${vao2}cm.`;
+    }
+
+    else if (numeroDePortas == 6) {
+        const vao1 = (medidaPortas * 2) - 1.2;
+        const vao2 = (medidaInterna - vao1) / 2;
+        document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 3 vãos. O do meio com ${vao1}cm, outros dois com ${vao2}cm.`;
+    }
+
+    else if (numeroDePortas == 7) {
+        const vao1 = medidaPortas + 0.3;
+        const vao2 = (medidaPortas * 2) + 1.2;
+        document.querySelector('.resultadoVaos').innerHTML = `O móvel terá 4 vãos. Um lateral com ${vao1}cm, outros 3 iguais com ${vao2}cm.`;
+    }
+
+    else {
+        document.querySelector('.resultado').innerHTML = " "
+        document.querySelector('.resultadoVaos').innerHTML = "Valor inválido. Por Favor, indique um número válido."
+    }
+}
